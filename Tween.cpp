3a244b8b12428e185d5356c442e1f2d640dcf268
Rune Skjoldborg Madsen <rune@runemadsen.com>
2010-11-19 21:30:16 -0500

    Finished prototype. Need function pointer instead of huge if statement

--- Tween.cpp
+++ Tween.cpp
@@ -1,17 +1,164 @@
 #include "Tween.h"
 
+/*	Constructor
+ ___________________________________________________ */
+
 Tween::Tween()
 {
-	ofBackground(0, 0, 0);
+	num = 0;
+	percent = 0;
+}
+
+/*	Setup methods
+ ___________________________________________________ */
+
+void Tween::setup(int duration, float start, float change, string ease, int delay, int loop)
+{
+	_tween.duration = duration;
+	_tween.loop = loop;
+	_delay.duration = delay;
 	
-	_img.loadImage("typeyourname.png");
-	_font.loadFont("GothamRounded-Book.ttf", 26, true, true);
+	_start = start;
+	_change = change;
 	
-	_nameCounter.duration = 200;
-	_titleFade.duration = 100;
-	_nameFade.duration = 100;
+	num = start;
+	_ease = ease;
+}
+
+void Tween::setup(int duration, float start, float change, string ease)
+{
+	setup(duration, start, change, ease, 0, 0);
+}
+
+void Tween::setup(int duration, float start, float change, string ease, int delay)
+{
+	setup(duration, start, change, ease, delay, 0);
+}
+
+/*	Update
+ ___________________________________________________ */
+
+void Tween::update()
+{
+	if(_delay.duration > 0)
+	{
+		_delay.tick();
+		
+		if(_delay.time == _delay.duration)
+		{
+			_tween.state = 1;
+		}
+	}
 	
-	_userName = "";
+	_tween.tick();
 	
-	_fadeMode = false;
+	percent = _tween.time / _tween.duration;
+
+	runEasing();
+}
+
+void Tween::runEasing()
+{
+	if (_ease == BACK_EASEIN)
+		num = Back::easeIn(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == BACK_EASEOUT)
+		num = Back::easeOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == BACK_EASEINOUT)
+		num = Back::easeInOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == BOUNCE_EASEIN)
+		num = Bounce::easeIn(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == BOUNCE_EASEOUT)
+		num = Bounce::easeOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == BOUNCE_EASEINOUT)
+		num = Bounce::easeInOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == CIRC_EASEIN)
+		num = Circ::easeIn(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == CIRC_EASEOUT)
+		num = Circ::easeOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == CIRC_EASEINOUT)
+		num = Circ::easeInOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == CUBIC_EASEIN)
+		num = Cubic::easeIn(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == CUBIC_EASEOUT)
+		num = Cubic::easeOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == CUBIC_EASEINOUT)
+		num = Cubic::easeInOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == ELASTIC_EASEIN)
+		num = Elastic::easeIn(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == ELASTIC_EASEOUT)
+		num = Elastic::easeOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == ELASTIC_EASEINOUT)
+		num = Elastic::easeInOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == EXPO_EASEIN)
+		num = Expo::easeIn(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == EXPO_EASEOUT)
+		num = Expo::easeOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == EXPO_EASEINOUT)
+		num = Expo::easeInOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == LINEAR_EASEIN)
+		num = Linear::easeIn(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == LINEAR_EASEOUT)
+		num = Linear::easeOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == LINEAR_EASEINOUT)
+		num = Linear::easeInOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == QUAD_EASEIN)
+		num = Quad::easeIn(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == QUAD_EASEOUT)
+		num = Quad::easeOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == QUAD_EASEINOUT)
+		num = Quad::easeInOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == QUART_EASEIN)
+		num = Quart::easeIn(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == QUART_EASEOUT)
+		num = Quart::easeOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == QUART_EASEINOUT)
+		num = Quart::easeInOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == QUINT_EASEIN)
+		num = Quint::easeIn(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == QUINT_EASEOUT)
+		num = Quint::easeOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == QUINT_EASEINOUT)
+		num = Quint::easeInOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == SINE_EASEIN)
+		num = Sine::easeIn(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == SINE_EASEOUT)
+		num = Sine::easeOut(_tween.time, _start, _change, _tween.duration);
+	else if(_ease == SINE_EASEINOUT)
+		num = Sine::easeInOut(_tween.time, _start, _change, _tween.duration);
+	else 	
+		cout << "ERROR: Wrong tween specfied\n";
+}
+
+/*	Control methods
+ ___________________________________________________ */
+
+void Tween::play()
+{
+	if(_delay.duration > 0 && _delay.time != _delay.duration)
+	{
+		_delay.state = 1;
+	}
+	else 
+	{
+		_tween.state = 1;
+	}
+}
+
+void Tween::pause()
+{
+	_delay.state = 0;
+	_tween.state = 0;
+}
+
+void Tween::stop()
+{
+	pause();
+	
+	_delay.time = 0;
+	_tween.time = 0;
+}
+
+bool Tween::finished()
+{
+	return _tween.time == _tween.duration ? true : false;
 }
--- Tween.h
+++ Tween.h
@@ -13,6 +13,96 @@
 #include "Quint.h"
 #include "Sine.h"
 
+#define BACK_EASEIN "Back.easeIn"
+#define BACK_EASEOUT "Back.easeOut"
+#define BACK_EASEINOUT "Back.easeInOut"
+
+#define BOUNCE_EASEIN "Bounce.easeIn"
+#define BOUNCE_EASEOUT "Bounce.easeOut"
+#define BOUNCE_EASEINOUT "Bounce.easeInOut"
+
+#define CIRC_EASEIN "Circ.easeIn"
+#define CIRC_EASEOUT "Circ.easeOut"
+#define CIRC_EASEINOUT "Circ.easeInOut"
+
+#define CUBIC_EASEIN "Cubic.easeIn"
+#define CUBIC_EASEOUT "Cubic.easeOut"
+#define CUBIC_EASEINOUT "Cubic.easeInOut"
+
+#define ELASTIC_EASEIN "Elastic.easeIn"
+#define ELASTIC_EASEOUT "Elastic.easeOut"
+#define ELASTIC_EASEINOUT "Elastic.easeInOut"
+
+#define EXPO_EASEIN "Expo.easeIn"
+#define EXPO_EASEOUT "Expo.easeOut"
+#define EXPO_EASEINOUT "Expo.easeInOut"
+
+#define LINEAR_EASEIN "Linear.easeIn"
+#define LINEAR_EASEOUT "Linear.easeOut"
+#define LINEAR_EASEINOUT "Linear.easeInOut"
+
+#define QUAD_EASEIN "Quad.easeIn"
+#define QUAD_EASEOUT "Quad.easeOut"
+#define QUAD_EASEINOUT "Quad.easeInOut"
+
+#define QUART_EASEIN "Quart.easeIn"
+#define QUART_EASEOUT "Quart.easeOut"
+#define QUART_EASEINOUT "Quart.easeInOut"
+
+#define QUINT_EASEIN "qQuint.easeIn"
+#define QUINT_EASEOUT "Quint.easeOut"
+#define QUINT_EASEINOUT "Quint.easeInOut"
+
+#define SINE_EASEIN "Sine.easeIn"
+#define SINE_EASEOUT "Sine.easeOut"
+#define SINE_EASEINOUT "Sine.easeInOut"
+
+/*	TweenTimer
+ ___________________________________________________ */
+
+class TweenTimer 
+{
+public:
+	
+	int state; 
+	float time, duration;
+	int loop;
+	
+	TweenTimer() 
+	{
+		loop = 0;
+		time = 0.0; 
+		duration = 0.0; 
+		state = 0;
+	}
+	
+	void tick()
+	{
+		time += state;
+		
+		if (time > duration) 
+		{
+			time = duration;
+			
+			if(loop == 0)
+				state = 0;
+			else if(loop == 1)
+				state = time = 0;
+			else if(loop == 2)
+				state -1;
+		}
+		else if(time < 0)
+		{
+			time = 0;
+			
+			if(loop == 0)
+				state = 0;
+			else if(loop == 2)
+				state = 1;
+		}
+	}
+};
+
 /*	Tween
  ___________________________________________________ */
 
@@ -24,52 +114,25 @@ public:
 	void setup(int duration, float start, float change, string ease);
 	void setup(int duration, float start, float change, string ease, int delay);
 	void setup(int duration, float start, float change, string ease, int delay, int loop);
-	void start();
+	void play();
 	void pause();
 	void stop();
+	
 	bool finished();
-	bool tweening();
 	
-	float get
+	void update();
 	
+	float num;
+	float percent;
 	
 private:
 	
-	TweenTimer _timer;
-};
-
-/*	TweenTimer
- ___________________________________________________ */
-
-class TweenTimer 
-{
-public:
+	TweenTimer _tween;
+	TweenTimer _delay;
 	
-	int state; 
-	float time, duration;
-	bool upDown;
+	float _start;
+	float _change;
+	string _ease;
 	
-	TweenTimer() 
-	{
-		upDown = false;
-		time = 0.0; 
-		duration = 0.0; 
-		state = 1;
-	}
-	
-	void tick()
-	{
-		time += state;
-		
-		if (time > duration) 
-		{
-			time = duration;
-			state = upDown ? -1 : 0;
-		}
-		else if(time < 0)
-		{
-			time = 0;
-			state = upDown ? 1 : 0;
-		}
-	}
+	void runEasing();
 };
